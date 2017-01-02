6d6860314a649765f063aead547e59b34c253b65
ArrayOne <1441576840@qq.com>
2017-01-02 11:16:45 +0800

    2：配置路由
    app.config(function($routeProvider){
    $routeProvide.when('路由地址'，‘对应的html页面’)
    })

--- js/kaifanla.js
+++ js/kaifanla.js
@@ -1,67 +1,16 @@
-/**
- * Created by bjwsl-001 on 2016/12/30.
- */
+//建立模板空间
+var app=angular.module('myApp',['ng','ngRoute']);
 
-var app = angular.module('kaifanla',['ng','ngRoute']);
-
-app.config(function ($routeProvider) {
-
-//  添加路由
+//配置路由
+app.config(function($routeProvider){
   $routeProvider
-    .when('/start',{
-      templateUrl:'tpl/start.html',
-      controller:'startCtrl'
-    })
-    .when('/main',{
-      templateUrl:'tpl/main.html',
-      controller:'mainCtrl'
-    })
-    .when('/detail',{
-      templateUrl:'tpl/detail.html',
-      controller:'detailCtrl'
-    })
-    .when('/order',{
-      templateUrl:'tpl/order.html',
-      controller:'orderCtrl'
-    })
-    .when('/myOrder',{
-      templateUrl:'tpl/myOrder.html',
-      controller:'myOrderCtrl'
-    })
+    .when('/detail',{ templateUrl:'tpl/detail.html'})
+    .when('/main',{templateUrl:'tpl/main.html'})
+    .when('/myOrder',{templateUrl:'tpl/myOrder.html'})
+    .when('/order',{templateUrl:'tpl/order.html'})
+    .when('/start',{templateUrl:'tpl/start.html'})
+    .otherwise({redirectTo:'/start'})
     .otherwise({redirectTo:'/start'});
-})
+});
 
-app.controller('parentCtrl',
-  ['$scope','$location', function ($scope,$location) {
-
-    $scope.jump = function (arg) {
-      $location.path(arg);
-    }
-
-}]);
-
-
-app.controller('startCtrl',
-  ['$scope', function ($scope) {
-
-}]);
-
-app.controller('mainCtrl',
-  ['$scope', function ($scope) {
-
-  }]);
-
-app.controller('detailCtrl',
-  ['$scope', function ($scope) {
-
-  }]);
-
-app.controller('orderCtrl',
-  ['$scope', function ($scope) {
-
-  }]);
-
-app.controller('myOrderCtrl',
-  ['$scope', function ($scope) {
-
-  }]);
+//建立一个父级控制器，便于页面之间的相互跳转
