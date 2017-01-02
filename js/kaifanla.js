dc637d2ae744f14d37edd09f0c09de1218da6395
ArrayOne <1441576840@qq.com>
2017-01-02 11:25:59 +0800

    3：建立控制器
    建立一个父级控制器：便于页面跳转。
    单独为每个模块建立控制器：便于后面的数据操作

--- js/kaifanla.js
+++ js/kaifanla.js
@@ -1,7 +1,7 @@
-//建立模板空间
+//1建立模板空间
 var app=angular.module('myApp',['ng','ngRoute']);
 
-//配置路由
+//2配置路由
 app.config(function($routeProvider){
   $routeProvider
     .when('/detail',{ templateUrl:'tpl/detail.html'})
@@ -13,4 +13,39 @@ app.config(function($routeProvider){
     .otherwise({redirectTo:'/start'});
 });
 
-//建立一个父级控制器，便于页面之间的相互跳转
+//3建立一个父级控制器，便于页面之间的相互跳转(行内注入，在文件压缩时候不易出错)
+app.controller('parentCtrl',['$scope','$location',function($scope,$location){
+ /*定义一个jump方法，用于跳转页面*/
+  $scope.jump=function(arg){
+    $location.href=arg;
+  }
+}]);
+
+//4分别为每个模块，建立控制器，便于数据操作
+
+app.controller('detailCtrl',['$scope',function(){
+
+
+}]);
+
+app.controller('mainCtrl',['$scope',function(){
+
+
+}]);
+
+app.controller('myOrderCtrl',['$scope',function(){
+
+
+}]);
+
+app.controller('orderCtrl',['$scope',function(){
+
+
+}]);
+
+app.controller('startCtrl',['$scope',function(){
+
+
+}]);
+
+
